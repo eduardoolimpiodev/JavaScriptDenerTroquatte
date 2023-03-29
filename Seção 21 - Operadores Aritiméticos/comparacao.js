ab414314fec8f11d66edd04810e41665ba8957b8
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-03-29 17:13:11 -0300

    add: about comparação

---
+++ Seção 21 - Operadores Aritiméticos/comparacao.js
@@ -0,0 +1,15 @@
+/* 
+==, ===, !=, !==, >, <, >=, <=
+*/
+
+console.log(1 == 1); // true
+console.log(1 === 1); // true
+console.log(1 == '1'); // true
+console.log(1 === '1'); //false
+
+console.log(1 != 1); //false
+console.log(1 != '1'); //false
+console.log(1 !== '1'); //true
+
+console.log(1 >= 2); //false
+console.log(3 > 2); //true
