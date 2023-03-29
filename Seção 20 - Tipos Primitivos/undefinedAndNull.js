5563a8d9ee594de56113a62ce33b06735449a34b
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-03-29 14:00:53 -0300

    add: about null and Undefined

---
+++ Seção 20 - Tipos Primitivos/undefinedAndNull.js
@@ -0,0 +1,7 @@
+
+console.log(undefined); // Sem definição até o momento.
+console.log(null); // está vazio, ou seja, nulo.
+
+// null é diferente de undefined:
+
+console.log(null === undefined); //false
