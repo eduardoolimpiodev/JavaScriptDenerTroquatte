fb21dd98d1c716f2e6ebae5df43030f32e7cbce3
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-03-29 14:39:07 -0300

    add: about array

---
+++ Seção 20 - Tipos Primitivos/array.js
@@ -0,0 +1,6 @@
+console.log(['Eduardo', 'Maria', 'Marcele']);
+console.log(['Eduardo', 'Maria', 'Marcele'].length);
+
+['Eduardo', 'Maria', 'Marcele'].forEach((res, index) => {
+    console.log(res, index)
+});
