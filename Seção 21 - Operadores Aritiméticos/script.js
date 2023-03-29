14deb5afc2a770c09ec747de09b68f1f9c870846
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-03-29 15:58:28 -0300

    add: about multiplicação

--- Seção 21 - Operadores Aritiméticos/script.js
+++ Seção 21 - Operadores Aritiméticos/script.js
@@ -18,3 +18,9 @@ console.log('Eduardo ' + 'Olimpio' + ',' + ' idade: ' + 36 + ' anos.' );
 
 console.log(1 - 1);
 console.log('1' - 1);
+
+//Multiplicação:
+
+console.log(2 * 10);
+console.log(10 * 5);
+
