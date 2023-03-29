bf969ba9f2a16f10e952e6b30d9ace189e3c3e10
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-03-29 15:55:36 -0300

    add: about adição

---
+++ Seção 21 - Operadores Aritiméticos/script.js
@@ -0,0 +1,18 @@
+/* 
+
+Adição +
+Subtração -
+Multiplicação *
+Divisão /
+Resto da Divisão %
+Exponenciação **
+*/
+
+//Adição/Concatenação:
+console.log(1 + 1);
+console.log('Eduardo ' + 'Olimpio'); //Concatenação
+
+console.log('Eduardo ' + 'Olimpio' + ',' + ' idade: ' + 36 + ' anos.' );
+
+//Subtração:
+
