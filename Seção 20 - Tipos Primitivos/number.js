b02486f5b6d4db5a0cd3da4431edb458412538fa
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-03-29 13:55:04 -0300

    add: about Numbers

---
+++ Seção 20 - Tipos Primitivos/number.js
@@ -0,0 +1,15 @@
+console.log(123);
+console.log("123");
+console.log("123" + 1);
+
+console.log("123" - 1);
+console.log(Number("123") + 1);
+
+console.log("123" * 2);
+console.log(Number("123") * 2);
+
+console.log(typeof 123);
+console.log(typeof Number("123"));
+console.log(typeof "123");
+console.log(typeof("123" + 1));
+
