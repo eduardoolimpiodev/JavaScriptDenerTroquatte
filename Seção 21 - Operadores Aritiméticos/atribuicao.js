6224eacc6c9b1111a8904ac0072b155d66d9cd24
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-03-29 16:09:00 -0300

    add: about comparação

---
+++ Seção 21 - Operadores Aritiméticos/atribuicao.js
@@ -0,0 +1,22 @@
+/* 
+    =, +=, -=, *=
+*/
+
+let a = 1;
+let b = 2;
+
+console.log(a);
+console.log(a + b);
+
+a += 1;
+console.log(a); //2
+
+a -= 1;
+console.log(a); //1
+
+a *= 2;
+console.log(a); //2
+
+a /= 2;
+console.log(a); //1
+
