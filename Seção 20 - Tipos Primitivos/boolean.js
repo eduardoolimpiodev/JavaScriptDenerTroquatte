7047a3a61a7cabbb19706d4ad7cb01507cc7ff4d
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2023-03-29 13:57:39 -0300

    add: about booleans

---
+++ Seção 20 - Tipos Primitivos/boolean.js
@@ -0,0 +1,10 @@
+/* Boolean : verdadeiro ou falso / True or False */
+
+console.log(typeof false);
+console.log(true);
+
+console.log(1 === 2);
+console.log(2 === 2);
+
+console.log(2 == '2');
+console.log(2 === '2');
